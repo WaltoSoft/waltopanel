b76cbf6b1975c9f1a600b5a3d4788beb064e06cf
Billy Walton <waltosoft@gmail.com>
2025-09-03 00:12:40 -0500

    Added first attempt at multi monitor support.

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,10 @@
 use crate::CurtainBar;
 use gtk::gio;
 use gtk::prelude::*;
+use gtk::gdk;
+use gtk4_layer_shell::LayerShell;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
   adw::init().unwrap();
@@ -19,20 +23,82 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     .flags(gio::ApplicationFlags::empty())
     .build();
 
-  app.connect_activate(|app| {
-    if let Err(e) = build_panel(app) {
-      eprintln!("Failed to build panel: {}", e);
-      std::process::exit(1);
-    }
-  });
+  let panels = Rc::new(RefCell::new(Vec::<CurtainBar>::new()));
+  
+  {
+    let panels = panels.clone();
+    app.connect_activate(move |app| {
+      if let Err(e) = build_panels_for_all_monitors(app, &panels) {
+        eprintln!("Failed to build panels: {}", e);
+        std::process::exit(1);
+      }
+    });
+  }
 
   app.run_with_args(&Vec::<String>::new());
   Ok(())
 }
 
-fn build_panel(app: &adw::Application) -> Result<(), Box<dyn std::error::Error>> {
-  let mut panel = CurtainBar::new(app)?;
-  panel.add_sample_menus();
-  panel.present();
+fn build_panels_for_all_monitors(
+  app: &adw::Application, 
+  panels: &Rc<RefCell<Vec<CurtainBar>>>
+) -> Result<(), Box<dyn std::error::Error>> {
+  let display = gdk::Display::default().ok_or("Could not get default display")?;
+  
+  // Clear existing panels
+  panels.borrow_mut().clear();
+  
+  // Create panel for each monitor
+  let n_monitors = display.monitors().n_items();
+  println!("Found {} monitors", n_monitors);
+  
+  for i in 0..n_monitors {
+    if let Some(monitor_obj) = display.monitors().item(i) {
+      let monitor = monitor_obj.downcast::<gdk::Monitor>()
+        .map_err(|_| "Failed to downcast monitor")?;
+      
+      println!("Creating panel for monitor {}: {:?}", i, monitor.model());
+      
+      let mut panel = create_panel_for_monitor(app, &monitor, i as usize)?;
+      panel.add_sample_menus_for_monitor(i as usize);
+      panel.present();
+      
+      panels.borrow_mut().push(panel);
+    }
+  }
+  
+  // Set up monitor change handling
+  {
+    let panels = panels.clone();
+    let app_weak = app.downgrade();
+    let monitors = display.monitors();
+    monitors.connect_items_changed(move |_, _, _, _| {
+      println!("Monitors changed, recreating panels");
+      if let Some(app) = app_weak.upgrade() {
+        if let Err(e) = build_panels_for_all_monitors(&app, &panels) {
+          eprintln!("Failed to recreate panels: {}", e);
+        }
+      }
+    });
+  }
+  
   Ok(())
+}
+
+fn create_panel_for_monitor(
+  app: &adw::Application, 
+  monitor: &gdk::Monitor,
+  monitor_index: usize
+) -> Result<CurtainBar, Box<dyn std::error::Error>> {
+  let geometry = monitor.geometry();
+  println!("Monitor geometry: {}x{} at ({}, {})", 
+           geometry.width(), geometry.height(), geometry.x(), geometry.y());
+  
+  // Create the panel
+  let panel = CurtainBar::new(app)?;
+  
+  // Explicitly assign this panel to the specific monitor
+  panel.window.set_monitor(Some(monitor));
+  
+  Ok(panel)
 }
--- src/curtain_bar.rs
+++ src/curtain_bar.rs
@@ -48,7 +48,7 @@ impl CurtainBarBuilder {
 }
 
 pub struct CurtainBar {
-  window: gtk::ApplicationWindow,
+  pub window: gtk::ApplicationWindow,
   left_box: gtk::Box,
   center_box: gtk::Box,
   right_box: gtk::Box,
@@ -184,14 +184,37 @@ impl CurtainBar {
   }
 
   pub fn add_sample_menus(&mut self) {
+    self.add_sample_menus_for_monitor(0);
+  }
+
+  pub fn add_sample_menus_for_monitor(&mut self, monitor_index: usize) {
+    match monitor_index {
+      0 => self.add_primary_monitor_menus(),
+      1 => self.add_secondary_monitor_menus(),
+      _ => self.add_additional_monitor_menus(monitor_index),
+    }
+  }
+
+  fn add_primary_monitor_menus(&mut self) {
+    println!("Adding primary monitor menus (File, Edit, View)");
+    
     // File menu
     let file_menu = DropdownMenuButton::builder()
       .with_text("File")
       .build();
 
+    file_menu.connect_item_selected(|_, item_id| {
+      println!("Primary File item selected: {}", item_id);
+    });
+
+    file_menu.connect_item_toggled(|_, item_id, toggled_state| {
+      println!("Primary File item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
+    });
+
+    let file_menu = self.setup_dropdown(file_menu);
     let file_items = vec![
       MenuItem::new("new", "New File").with_icon("document-new-symbolic"),
-      MenuItem::new("open", "Open..."),
+      MenuItem::new("open", "Open...").with_icon("document-open-symbolic"),
       MenuItem::new("recent", "Recent Files")
         .with_icon("document-open-recent-symbolic")
         .with_submenu(vec![
@@ -200,41 +223,54 @@ impl CurtainBar {
           MenuItem::new("recent3", "style.css"),
         ]),
       MenuItem::separator(),
-      MenuItem::new("save", "Save").toggled(),
-      MenuItem::new("save_as", "Save As..."),
-      MenuItem::new("export", "Export"),
+      MenuItem::new("save", "Save").with_icon("document-save-symbolic"),
+      MenuItem::new("save_as", "Save As...").with_icon("document-save-as-symbolic"),
       MenuItem::separator(),
       MenuItem::new("quit", "Quit").with_icon("application-exit-symbolic"),
     ];
+    file_menu.set_menu_items(file_items);
+    self.add_widget_to_left(&file_menu);
+    self.dropdowns.borrow_mut().push(file_menu);
 
-    file_menu.connect_item_selected(|_, item_id| {
-      println!("File item selected: {}", item_id);
+    // Edit menu
+    let edit_menu = DropdownMenuButton::builder()
+      .with_text("Edit")
+      .build();
+
+    edit_menu.connect_item_selected(|_, item_id| {
+      println!("Primary Edit item selected: {}", item_id);
     });
 
-    file_menu.connect_item_toggled(|_, item_id, toggled_state| {
-      println!("File item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
-    });
-
-    let file_menu_setup = self.setup_dropdown(file_menu);
-    file_menu_setup.set_menu_items(file_items);
-    self.add_widget_to_left(&file_menu_setup);
-    self.dropdowns.borrow_mut().push(file_menu_setup);
+    let edit_menu = self.setup_dropdown(edit_menu);
+    let edit_items = vec![
+      MenuItem::new("undo", "Undo").with_icon("edit-undo-symbolic"),
+      MenuItem::new("redo", "Redo").with_icon("edit-redo-symbolic"),
+      MenuItem::separator(),
+      MenuItem::new("cut", "Cut").with_icon("edit-cut-symbolic"),
+      MenuItem::new("copy", "Copy").with_icon("edit-copy-symbolic"),
+      MenuItem::new("paste", "Paste").with_icon("edit-paste-symbolic"),
+      MenuItem::separator(),
+      MenuItem::new("find", "Find").with_icon("edit-find-symbolic"),
+      MenuItem::new("replace", "Replace").with_icon("edit-find-replace-symbolic"),
+    ];
+    edit_menu.set_menu_items(edit_items);
+    self.add_widget_to_left(&edit_menu);
+    self.dropdowns.borrow_mut().push(edit_menu);
 
     // View menu
     let view_menu = DropdownMenuButton::builder()
       .with_text("View")
       .build();
-    
+
     view_menu.connect_item_selected(|_, item_id| {
-      println!("View item selected: {}", item_id);
+      println!("Primary View item selected: {}", item_id);
     });
 
     view_menu.connect_item_toggled(|_, item_id, toggled_state| {
-      println!("View item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
+      println!("Primary View item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
     });
-    
-    let view_menu = self.setup_dropdown(view_menu);
 
+    let view_menu = self.setup_dropdown(view_menu);
     let view_items = vec![
       MenuItem::new("fullscreen", "Fullscreen").with_icon("view-fullscreen-symbolic"),
       MenuItem::new("zoom_in", "Zoom In").with_icon("zoom-in-symbolic"),
@@ -248,80 +284,155 @@ impl CurtainBar {
         .with_icon("view-dual-symbolic")
         .toggled(),
     ];
-
     view_menu.set_menu_items(view_items);
     self.add_widget_to_left(&view_menu);
     self.dropdowns.borrow_mut().push(view_menu);
+  }
 
-    // Settings menu with icon
-    let settings_menu = DropdownMenuButton::builder()
-      .with_icon_and_text("preferences-system-symbolic", "Settings")
+  fn add_secondary_monitor_menus(&mut self) {
+    println!("Adding secondary monitor menus (Tools, Debug, Help)");
+
+    // Tools menu
+    let tools_menu = DropdownMenuButton::builder()
+      .with_icon_and_text("applications-development-symbolic", "Tools")
       .build();
-    
-    settings_menu.connect_item_selected(|_, item_id| {
-      println!("Settings item selected: {}", item_id);
+
+    tools_menu.connect_item_selected(|_, item_id| {
+      println!("Secondary Tools item selected: {}", item_id);
     });
 
-    settings_menu.connect_item_toggled(|_, item_id, toggled_state| {
-      println!("Settings item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
-    });
-    
-    let settings_menu = self.setup_dropdown(settings_menu);
-
-    let settings_items = vec![
-      MenuItem::new("preferences", "Preferences...").with_icon("preferences-system-symbolic"),
-      MenuItem::new("keyboard", "Keyboard Shortcuts").with_icon("input-keyboard-symbolic"),
+    let tools_menu = self.setup_dropdown(tools_menu);
+    let tools_items = vec![
+      MenuItem::new("terminal", "Terminal").with_icon("utilities-terminal-symbolic"),
+      MenuItem::new("calculator", "Calculator").with_icon("accessories-calculator-symbolic"),
+      MenuItem::new("text_editor", "Text Editor").with_icon("accessories-text-editor-symbolic"),
       MenuItem::separator(),
-      MenuItem::new("plugins", "Plugins").with_icon("application-x-addon-symbolic"),
-      MenuItem::new("themes", "Themes")
-        .with_icon("preferences-desktop-theme-symbolic")
+      MenuItem::new("system_monitor", "System Monitor").with_icon("utilities-system-monitor-symbolic"),
+      MenuItem::new("disk_usage", "Disk Usage").with_icon("baobab-symbolic"),
+      MenuItem::new("network", "Network Tools")
+        .with_icon("network-wired-symbolic")
         .with_submenu(vec![
-          MenuItem::new("theme_light", "Light Theme"),
-          MenuItem::new("theme_dark", "Dark Theme").toggled(),
-          MenuItem::new("theme_auto", "Auto (System)"),
-          MenuItem::new("theme_custom", "Custom Themes").with_submenu(vec![
-            MenuItem::new("theme_blue", "Blue Theme"),
-            MenuItem::new("theme_green", "Green Theme"),
-            MenuItem::new("theme_advanced", "Advanced").with_submenu(vec![
-              MenuItem::new("theme_editor", "Theme Editor"),
-              MenuItem::new("theme_import", "Import Theme"),
-              MenuItem::new("theme_export", "Export Theme"),
-            ]),
-          ]),
+          MenuItem::new("ping", "Ping"),
+          MenuItem::new("traceroute", "Traceroute"),
+          MenuItem::new("netstat", "Network Status"),
         ]),
     ];
+    tools_menu.set_menu_items(tools_items);
+    self.add_widget_to_left(&tools_menu);
+    self.dropdowns.borrow_mut().push(tools_menu);
 
-    settings_menu.set_menu_items(settings_items);
-    self.add_widget_to_right(&settings_menu);
-    self.dropdowns.borrow_mut().push(settings_menu);
-
-    // User menu (icon only)
-    let user_menu = DropdownMenuButton::builder()
-      .with_icon("avatar-default-symbolic")
+    // Debug menu
+    let debug_menu = DropdownMenuButton::builder()
+      .with_icon_and_text("applications-debugging-symbolic", "Debug")
       .build();
-    
-    user_menu.connect_item_selected(|_, item_id| {
-      println!("User item selected: {}", item_id);
+
+    debug_menu.connect_item_selected(|_, item_id| {
+      println!("Secondary Debug item selected: {}", item_id);
     });
 
-    user_menu.connect_item_toggled(|_, item_id, toggled_state| {
-      println!("User item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
+    debug_menu.connect_item_toggled(|_, item_id, toggled_state| {
+      println!("Secondary Debug item toggled: {} ({})", item_id, if toggled_state { "ON" } else { "OFF" });
     });
-    
-    let user_menu = self.setup_dropdown(user_menu);
 
-    let user_items = vec![
-      MenuItem::new("profile", "Profile").with_icon("user-info-symbolic"),
-      MenuItem::new("account", "Account Settings").with_icon("system-users-symbolic"),
+    let debug_menu = self.setup_dropdown(debug_menu);
+    let debug_items = vec![
+      MenuItem::new("start_debug", "Start Debugging").with_icon("media-playback-start-symbolic"),
+      MenuItem::new("stop_debug", "Stop Debugging").with_icon("media-playback-stop-symbolic"),
       MenuItem::separator(),
-      MenuItem::new("help", "Help & Support").with_icon("help-browser-symbolic"),
-      MenuItem::new("about", "About").with_icon("help-about-symbolic"),
+      MenuItem::new("breakpoint", "Toggle Breakpoint").with_icon("media-record-symbolic"),
+      MenuItem::new("step_over", "Step Over").with_icon("go-next-symbolic"),
+      MenuItem::new("step_into", "Step Into").with_icon("go-down-symbolic"),
       MenuItem::separator(),
-      MenuItem::new("logout", "Log Out").with_icon("system-log-out-symbolic"),
+      MenuItem::new("verbose_logging", "Verbose Logging")
+        .with_icon("text-x-generic-symbolic")
+        .toggled(),
     ];
+    debug_menu.set_menu_items(debug_items);
+    self.add_widget_to_left(&debug_menu);
+    self.dropdowns.borrow_mut().push(debug_menu);
 
-    user_menu.set_menu_items(user_items);
-    self.add_widget_to_right(&user_menu);
-    self.dropdowns.borrow_mut().push(user_menu);
+    // Help menu on the right
+    let help_menu = DropdownMenuButton::builder()
+      .with_icon("help-browser-symbolic")
+      .build();
+
+    help_menu.connect_item_selected(|_, item_id| {
+      println!("Secondary Help item selected: {}", item_id);
+    });
+
+    let help_menu = self.setup_dropdown(help_menu);
+    let help_items = vec![
+      MenuItem::new("documentation", "Documentation").with_icon("help-contents-symbolic"),
+      MenuItem::new("shortcuts", "Keyboard Shortcuts").with_icon("input-keyboard-symbolic"),
+      MenuItem::new("tutorial", "Tutorial").with_icon("applications-education-symbolic"),
+      MenuItem::separator(),
+      MenuItem::new("report_bug", "Report Bug").with_icon("tools-report-bug-symbolic"),
+      MenuItem::new("about", "About").with_icon("help-about-symbolic"),
+    ];
+    help_menu.set_menu_items(help_items);
+    self.add_widget_to_right(&help_menu);
+    self.dropdowns.borrow_mut().push(help_menu);
+  }
+
+  fn add_additional_monitor_menus(&mut self, monitor_index: usize) {
+    println!("Adding additional monitor {} menus (System, Monitor Info)", monitor_index);
+
+    // System menu
+    let system_menu = DropdownMenuButton::builder()
+      .with_icon_and_text("computer-symbolic", &format!("System {}", monitor_index))
+      .build();
+
+    system_menu.connect_item_selected(move |_, item_id| {
+      println!("Monitor {} System item selected: {}", monitor_index, item_id);
+    });
+
+    system_menu.connect_item_toggled(move |_, item_id, toggled_state| {
+      println!("Monitor {} System item toggled: {} ({})", monitor_index, item_id, if toggled_state { "ON" } else { "OFF" });
+    });
+
+    let system_menu = self.setup_dropdown(system_menu);
+    let system_items = vec![
+      MenuItem::new("system_info", "System Information").with_icon("dialog-information-symbolic"),
+      MenuItem::new("processes", "Processes").with_icon("utilities-system-monitor-symbolic"),
+      MenuItem::new("services", "Services").with_icon("preferences-system-services-symbolic"),
+      MenuItem::separator(),
+      MenuItem::new("power", "Power Options")
+        .with_icon("system-shutdown-symbolic")
+        .with_submenu(vec![
+          MenuItem::new("suspend", "Suspend"),
+          MenuItem::new("hibernate", "Hibernate"),
+          MenuItem::new("restart", "Restart"),
+          MenuItem::new("shutdown", "Shutdown"),
+        ]),
+      MenuItem::separator(),
+      MenuItem::new("auto_update", "Auto Updates")
+        .with_icon("software-update-available-symbolic")
+        .toggled(),
+    ];
+    system_menu.set_menu_items(system_items);
+    self.add_widget_to_left(&system_menu);
+    self.dropdowns.borrow_mut().push(system_menu);
+
+    // Monitor info on the right
+    let monitor_menu = DropdownMenuButton::builder()
+      .with_icon_and_text("video-display-symbolic", &format!("Monitor {}", monitor_index))
+      .build();
+
+    monitor_menu.connect_item_selected(move |_, item_id| {
+      println!("Monitor {} info item selected: {}", monitor_index, item_id);
+    });
+
+    let monitor_menu = self.setup_dropdown(monitor_menu);
+    let monitor_items = vec![
+      MenuItem::new("display_settings", "Display Settings").with_icon("preferences-desktop-display-symbolic"),
+      MenuItem::new("resolution", "Resolution").with_icon("preferences-desktop-screensaver-symbolic"),
+      MenuItem::new("brightness", "Brightness").with_icon("display-brightness-symbolic"),
+      MenuItem::separator(),
+      MenuItem::new("wallpaper", "Wallpaper").with_icon("preferences-desktop-wallpaper-symbolic"),
+      MenuItem::new("screensaver", "Screensaver").with_icon("preferences-desktop-screensaver-symbolic"),
+    ];
+    monitor_menu.set_menu_items(monitor_items);
+    self.add_widget_to_right(&monitor_menu);
+    self.dropdowns.borrow_mut().push(monitor_menu);
   }
 }
